9366ab2a19e3ffbbfa32f798fe2c82ae4909cfe6
ukdave <github@uk-dave.com>
2017-12-12 21:39:42 +0000

    Day 1 part 2

--- lib/day1.rb
+++ lib/day1.rb
@@ -5,4 +5,15 @@ class Day1
     digits = input.chars.map(&:to_i)
     digits.push(digits.first).each_cons(2).reduce(0) {|acc, (a, b)| a == b ? acc + a : acc }
   end
+
+  def solve2 input
+    digits = input.chars.map(&:to_i)
+    digits.each_with_index.select {|_, i| digits[i] == digits[wrap(i, digits.length)] }.map(&:first).reduce(0, &:+)
+  end
+
+  private
+
+  def wrap idx, len
+    (idx + (len / 2)) % len
+  end
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -12,4 +12,15 @@ describe Day1 do
       expect(subject.solve(File.read("fixtures/day1.txt").strip)).to eq 1089
     end
   end
+
+  describe "#solve2" do
+    it "solves captchas" do
+      expect(subject.solve2("1212")).to eq 6
+      expect(subject.solve2("1221")).to eq 0
+      expect(subject.solve2("123425")).to eq 4
+      expect(subject.solve2("123123")).to eq 12
+      expect(subject.solve2("12131415")).to eq 4
+      expect(subject.solve2(File.read("fixtures/day1.txt").strip)).to eq 1156
+    end
+  end
 end
