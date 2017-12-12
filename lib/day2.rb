f538fc0510ce014aabeee0f54ce2f72175c2fa02
ukdave <github@uk-dave.com>
2017-12-12 22:30:17 +0000

    Day 2 part 2

--- lib/day2.rb
+++ lib/day2.rb
@@ -6,9 +6,28 @@ class Day2
     rows.map {|row| row_diff(row) }.reduce(0, &:+)
   end
 
+  def checksum2 lines
+    rows = lines.map {|line| line.split.map(&:to_i) }
+    rows.map {|row|
+      divs = row_divisibles(row)
+      divs.first / divs.last
+    }.reduce(0, &:+)
+  end
+
   private
 
   def row_diff row
     row.minmax.reduce(&:-).abs
+  end
+
+  def row_divisibles row
+    row.flat_map {|d|
+      div = divisor(d, row - [d])
+      div.nil? ? nil : [div, d]
+    }.compact
+  end
+
+  def divisor num, arr
+    arr.find {|x| (x % num).zero? }
   end
 end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -9,4 +9,11 @@ describe Day2 do
       expect(subject.checksum(File.readlines("fixtures/day2.txt"))).to eq 42_378
     end
   end
+
+  describe "#checksum2" do
+    it "calculates the checksum of the spreadsheet" do
+      expect(subject.checksum2(["5 9 2 8", "9 4 7 3", "3 8 6 5"])).to eq 9
+      expect(subject.checksum2(File.readlines("fixtures/day2.txt"))).to eq 246
+    end
+  end
 end
