2db8c372b13ce2e00f23d7450f1bfe82468f948f
ukdave <github@uk-dave.com>
2017-12-14 22:17:06 +0000

    Day 5 part 2

--- lib/day5.rb
+++ lib/day5.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Day5
+  # rubocop:disable Metrics/MethodLength
   def steps_to_escape maze_str
     maze = maze_str.map(&:to_i)
     pos = 0
@@ -13,4 +14,20 @@ class Day5
     end
     moves
   end
+  # rubocop:enable all
+
+  # rubocop:disable Metrics/MethodLength
+  def steps_to_escape_2 maze_str
+    maze = maze_str.map(&:to_i)
+    pos = 0
+    moves = 0
+    while pos < maze.length
+      steps = maze[pos]
+      maze[pos] += (steps >= 3 ? -1 : 1)
+      pos += steps
+      moves += 1
+    end
+    moves
+  end
+  # rubocop:enable all
 end
--- spec/day5_spec.rb
+++ spec/day5_spec.rb
@@ -9,4 +9,11 @@ describe Day5 do
       expect(subject.steps_to_escape(File.readlines("fixtures/day5.txt"))).to eq 343_467
     end
   end
+
+  describe "#steps_to_escape_2" do
+    it "returns the number of jumps to escape the maze" do
+      expect(subject.steps_to_escape_2(["0", "3", "0", "1", "-3"])).to eq 10
+      expect(subject.steps_to_escape_2(File.readlines("fixtures/day5.txt"))).to eq 24_774_780
+    end
+  end
 end
