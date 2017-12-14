53e58a18cdbe006f033198acd54c75b6815cb7a3
ukdave <github@uk-dave.com>
2017-12-14 21:21:59 +0000

    Day 4 part 2

--- lib/day4.rb
+++ lib/day4.rb
@@ -9,4 +9,14 @@ class Day4
   def count_valid_passphrases passphrasses
     passphrasses.select {|p| valid_passphrase? p }.count
   end
+
+  def valid_passphrase_2? passphrase
+    words = passphrase.split
+    sorted_words = words.map {|w| w.chars.sort.join }
+    sorted_words.uniq.length == sorted_words.length
+  end
+
+  def count_valid_passphrases_2 passphrasses
+    passphrasses.select {|p| valid_passphrase_2? p }.count
+  end
 end
--- spec/day4_spec.rb
+++ spec/day4_spec.rb
@@ -15,9 +15,29 @@ describe Day4 do
   end
 
   describe "count_valid_passphrases" do
-    it "returns the number of valid passphrasses" do
+    it "returns the number of valid passphrasses (that contain no duplicate words)" do
       expect(subject.count_valid_passphrases(["aa bb cc dd ee", "aa bb cc dd aaa", "aa bb cc dd aa"])).to eq 2
       expect(subject.count_valid_passphrases(File.readlines("fixtures/day4.txt"))).to eq 383
+    end
+  end
+
+  describe "#valid_passphrase_2?" do
+    it "returns true if there no words that are anagrams of eath other" do
+      expect(subject.valid_passphrase_2? "abcde fghij").to eq true
+      expect(subject.valid_passphrase_2? "a ab abc abd abf abj").to eq true
+      expect(subject.valid_passphrase_2? "iiii oiii ooii oooi oooo").to eq true
+    end
+
+    it "returns false if there are words that are anagrams of eath other" do
+      expect(subject.valid_passphrase_2? "abcde xyz ecdab").to eq false
+      expect(subject.valid_passphrase_2? "oiii ioii iioi iiio").to eq false
+    end
+  end
+
+  describe "count_valid_passphrases_2" do
+    it "returns the number of valid passphrasses (that contain no anagrams)" do
+      expect(subject.count_valid_passphrases_2(["abcde fghij", "a ab abc abd abf abj", "abcde xyz ecdab"])).to eq 2
+      expect(subject.count_valid_passphrases_2(File.readlines("fixtures/day4.txt"))).to eq 265
     end
   end
 end
